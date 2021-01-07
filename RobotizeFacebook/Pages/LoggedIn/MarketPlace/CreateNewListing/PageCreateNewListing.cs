dedb320170213c1f03abb41ef9bf928f385237bc
amhussain <amhussain7@gmail.com>
2021-01-06 20:49:13 -0800

    Saving draft changes as my computer is dying.

---
+++ RobotizeFacebook/Pages/LoggedIn/MarketPlace/CreateNewListing/PageCreateNewListing.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotizeFacebook.Pages.LoggedIn.MarketPlace.CreateNewListing
+{
+    public class PageCreateNewListing : PageBase
+    {
+        public override string PageUrl => "/marketplace/create";
+
+        public PageCreateNewListing(RemoteWebDriver driver, WebDriverWait wait)
+        {
+
+        }
+    }
+}
--- RobotizeFacebook/Pages/LoggedIn/MarketPlace/PageMarketplace.cs
+++ RobotizeFacebook/Pages/LoggedIn/MarketPlace/PageMarketplace.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using RobotizeToolbox.Controls.TriggerControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace RobotizeFacebook.Pages
 {
-    public class PageMarketplace
+    public class PageMarketplace : PageBase
     {
+        public override string PageUrl => "/marketplace";
+
+        public PageMarketplace(RemoteWebDriver driver, WebDriverWait wait)
+        {
+        }
+
+        public PageMarketplace()
+        {
+
+        }
+
+        public EventTriggerButton<> TriggerButtonCreateNewListing => new EventTriggerButton<EventResult>()
+        
     }
 }
