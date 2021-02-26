77b96a9e895447f63fe458a62e923c5a003ced1e
Ali <amhussain7@gmail.com>
2021-02-25 22:48:51 -0800

    Added appium project to the solution.

---
+++ RobotizeMobileFacebook/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotizeMobileFacebook
+{
+    class Program
+    {
+        private static AndroidDriver<AppiumWebElement> _driver;
+        private static AppiumLocalService _appiumLocalService;
+
+        static void Main(string[] args)
+        {
+            _appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+            _appiumLocalService.Start();
+            var appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Android_Accelerated_x86_Oreo");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "7.1");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.BrowserName, "Chrome");
+            _driver = new AndroidDriver<AppiumWebElement>(_appiumLocalService, appiumOptions);
+            _driver.CloseApp();
+        }
+    }
+}
