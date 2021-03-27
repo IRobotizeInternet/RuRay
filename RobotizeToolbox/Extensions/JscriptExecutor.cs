a62fc40cdc263f2cc964fa13debfaa300b894c8c
Ali <amhussain7@gmail.com>
2021-03-27 13:52:06 -0700

    Typescript is added to produce js

--- RobotizeToolbox/Extensions/JscriptExecutor.cs
+++ RobotizeToolbox/Extensions/JscriptExecutor.cs
@@ -80,25 +80,42 @@ namespace RobotizeToolbox.Extensions
                 return this.isElementOutViewport();";
 
 
-//            function isElementOutViewport()
-//            {
-//                var element = document.evaluate("//div[contains(@data-pagelet,'Feed')]//div[@aria-posinset='12']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-//                const rect = element.getBoundingClientRect();
-//                console.log(rect.top);
-//                console.log(rect.left);
-//                console.log(rect.bottom)
-//console.log((rect.bottom / 3) <= (window.innerHeight || document.documentElement.clientHeight));
-//                console.log(rect.right <= (window.innerWidth || document.documentElement.clientWidth));
-//                var a = (
-//                    rect.top >= -100 &&
-//                    rect.left >= 0 &&
-//                    (rect.bottom / 3) <= (window.innerHeight || document.documentElement.clientHeight) &&
-//                    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//                );
-//                console.log(a);
-//                return a;
-//            }
-//            this.isElementOutViewport();
+            //function isElementOutViewport()
+            //{
+            //    var element = document.evaluate("//div[contains(@data-pagelet,'Feed')]//div[@aria-posinset='4']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            //    const rect = element.getBoundingClientRect();
+            //    console.log(rect.top);
+            //    console.log(rect.bottom)
+            //        console.log((document.documentElement.clientHeight));
+            //    let hight = (window.innerHeight || document.documentElement.clientHeight);
+            //    let width = (window.innerWidth || document.documentElement.clientWidth);
+            //    var res = (
+            //        rect.top >= 0 &&
+            //        rect.left >= 0 &&
+            //        (rect.bottom) <= hight &&
+            //        rect.right <= width
+            //    );
+
+            //    let diff = 0;
+            //    if (res) return res;
+            //    else if (res == false && (rect.top < 0 && rect.bottom > hight)) return true;
+            //    else if (rect.top < 0)
+            //    {
+            //        diff = rect.bottom + rect.top;
+            //        console.log(diff);
+            //        console.log("Top");
+            //    }
+            //    else if (rect.bottom > hight)
+            //    {
+            //        diff = hight - rect.top;
+            //        console.log(diff);
+            //        console.log("Bottom");
+            //    }
+            //    console.log(hight / 2.5);
+            //    return diff >= hight / 2.5;
+            //}
+            //this.isElementOutViewport();
+
             try
             {
                 var js = (IJavaScriptExecutor)driver;
---
+++ RobotizeToolbox/TypeScriptFiles/TsFeedItem.js
@@ -0,0 +1,37 @@
+var TsFeedItem = /** @class */ (function () {
+    function TsFeedItem(_xpath) {
+        this._xpath = _xpath;
+    }
+    TsFeedItem.prototype.isElementOutViewport = function () {
+        var element = document.evaluate("" + this._xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        var rect = element.getBoundingClientRect();
+        console.log(rect.top);
+        console.log(rect.bottom);
+        console.log((document.documentElement.clientHeight));
+        var hight = (window.innerHeight || document.documentElement.clientHeight);
+        var width = (window.innerWidth || document.documentElement.clientWidth);
+        var res = (rect.top >= 0 &&
+            rect.left >= 0 &&
+            (rect.bottom) <= hight &&
+            rect.right <= width);
+        var diff = 0;
+        if (res)
+            return res;
+        else if (res == false && (rect.top < 0 && rect.bottom > hight))
+            return true;
+        else if (rect.top < 0) {
+            diff = rect.bottom + rect.top;
+            console.log(diff);
+            console.log("Top");
+        }
+        else if (rect.bottom > hight) {
+            diff = hight - rect.top;
+            console.log(diff);
+            console.log("Bottom");
+        }
+        console.log(hight / 2.5);
+        return diff >= hight / 2.5;
+    };
+    return TsFeedItem;
+}());
+//# sourceMappingURL=TsFeedItem.js.map
