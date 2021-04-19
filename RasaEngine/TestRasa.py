aa5cfaac3480143eb4dd7e7f7a989c52ce816b3a
Ali <amhussain7@gmail.com>
2021-04-18 23:17:58 -0700

    Removed rasa engine

--- RasaEngine/TestRasa.py
+++
@@ -1,7 +0,0 @@
-
-home_dir = os.chdir("c:\\Dev\\Robotize\\Rasa\\venv\\Scripts\\")
-home_dir = os.system("dir")
-print("`cd ~` ran with exit code %d" % home_dir)
-home_dir = os.system("activate")
-
-
