93ef9a903aafba22eaac1351a576e0aae08604ea
Ali <amhussain7@gmail.com>
2021-04-26 23:10:50 -0700

    Added tts

---
+++ RobotizeTTS/RobotizeTTS.py
@@ -0,0 +1,22 @@
+import pyttsx3
+engine = pyttsx3.init() # object creation
+
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+print (volume)                          #printing current volume level
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+engine.setProperty('rate', 350)     # setting up new voice rate
+engine.say("Hello World!")
+engine.say('My current speaking rate is ' + str(engine.getProperty('rate')))
+engine.runAndWait()
+engine.stop()
+
+"""Saving Voice to a file"""
+# On linux make sure that 'espeak' and 'ffmpeg' are installed
+engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
