1018a2a96aeaddf63081deb39d6672db829aba71
IRobotizeInternet <amhussain7@gmail.com>
2021-05-31 11:54:35 -0700

    Fixing typo

--- RuRay/RuRay.BLL/App/BasePage.cs
+++ RuRay/RuRay.BLL/App/BasePage.cs
@@ -11,13 +11,13 @@ namespace RuRayFacebook.App.LoggedIn.Pages
 
         /// <summary>
         /// This is the base element for all the popups. 
-        /// We can use this to see if there are any popus is opened on the page.
+        /// We can use this to see if there is any popus opened on the page.
         /// </summary>
         public string XPathForBasePopUp => "//div[@data-pagelet='page']/following-sibling::div/div";
 
         /// <summary>
         /// This is the base element for all the dialogs. 
-        /// We can use this to see if there are any dialog is opened on the page.
+        /// We can use this to see if there is any dialog opened on the page.
         /// </summary>
         public string XPathForBaseDialogs => "//div[@data-pagelet='root']/div/div[4]/div//div[@data-pagelet='root']//div[@role='dialog']";
         public virtual Header Header => new Header();
