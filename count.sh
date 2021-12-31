b703a02515f4eda726833c0145747a64ac976835
alir2t2 <irobotize@gmail.com>
2021-12-31 11:10:21 -0800

    updates

--- count.sh
+++ count.sh
@@ -1,20 +1,22 @@
-previousCommit=""
+Hash=""
 removed=0
 added=0
 totalCount=0
-for commit in $(git rev-list --reverse master --pretty='format:%as' | sed -z 's/\n/,/g;s/,$/\n/')
+totalChars=$(git ls-files | xargs cat | wc -l)
+echo $totalChars
+for commit in $(git rev-list master --pretty='format:%as' | sed -z 's/\n/,/g;s/,$/\n/')
 do
     hashAndCommit=`echo ${commit} | sed 's/,commit//g'`
     readarray -d , -t strarr <<<"$hashAndCommit"
     hash=${strarr[0]}
     commitDate=${strarr[1]}
    
-    if [ ! -z "$previousCommit" ]; then
-        removed=$(git diff --word-diff=porcelain --pretty="%H" $previousCommit..$hash | grep -e '^-[^-]' | wc -m)
-    fi
-    added=$(git diff --word-diff=porcelain --pretty="%H" $previousCommit..$hash | grep -e '^+[^+]' | wc -m)
-    ((totalCount = totalCount + removed - added))
-    echo $totalCount
-
-    previousCommit=$hash
+    if [ ! Hash" ]; then
+        removed=$(git diff --word-diff=porcelain --pretty="%H" $previousHash..$hash | grep -e '^-[^-]' | wc -m)
+        added=$(git diff --word-diff=porcelain --pretty="Hash..$hash | grep -e '^+[^+]' | wc -m)
+        ((totalChars = totalChars + added - removed))
+        echo [$commitDate, $totalChars]
+    else
+        added=$(git diff --word-diff=porcelain --pretty="Hash..$hash | grep -e '^+[^+]' | wc -m)
+    Hash=$hash
 done
