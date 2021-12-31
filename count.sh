f73aef562f1b1b38957603cb9be629f2de7ef6b0
alir2t2 <irobotize@gmail.com>
2021-12-31 01:19:16 -0800

    Clean up

--- count.sh
+++ count.sh
@@ -2,21 +2,19 @@ previousCommit=""
 removed=0
 added=0
 totalCount=0
-for commit in $(git rev-list --reverse master --pretty='format:%as' | sed -z 's/\n/,/g;s/,$/\n/')
+for commit in $(git rev-list  master --pretty='format:%as' | sed -z 's/\n/,/g;s/,$/\n/')
 do
     hashAndCommit=`echo ${commit} | sed 's/,commit//g'`
     readarray -d , -t strarr <<<"$hashAndCommit"
     hash=${strarr[0]}
     commitDate=${strarr[1]}
-    echo hello
-    echo hello+world
+   
     if [ ! -z "$previousCommit" ]; then
-        echo $previousCommit-kl
         removed=$(git diff --word-diff=porcelain --pretty="%H" $previousCommit..$hash | grep -e '^-[^-]' | wc -m)
-        added=$(git diff --word-diff=porcelain --pretty="%H" $previousCommit..$hash | grep -e '^+[^+]' | wc -m)
-        ((totalCount = total + removed - added))
-        echo $totalCount
     fi
+    added=$(git diff --word-diff=porcelain --pretty="%H" $previousCommit..$hash | grep -e '^+[^+]' | wc -m)
+    ((totalCount = total + removed - added))
+    echo $totalCount
 
     previousCommit=$hash
 done
